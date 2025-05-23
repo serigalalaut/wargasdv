a00db84bd5d63bc79c7da4577dc1f7a92f581712
fajar <fajarrhid@gmail.com>
2025-05-23 23:40:09 +0700

    fixing

--- web/resources/views/payment.blade.php
+++ web/resources/views/payment.blade.php
@@ -266,13 +266,14 @@
             <table class="table table-bordered">
                 <tr>
                     <th>Nomor Rumah</th>
-                    
+                    <th>Nominal</th>
                     <th>Status</th>
                     <th>Keterangan</th>
                 </tr>
                 @foreach ($list as $item)
                     <tr>
                         <td>{{ $item->home_no == 'B1-02' ? $item->home_no . " (Keamanan saja)": $item->home_no}}</td>
+                        <td>Rp. {{ number_format($item->nominal, 0, ',', '.') }}</td>
                         <td>{{ $item->status == 'Lunas' ? "Terkonfirmasi": "Belum Terkonfirmasi" }}</td>
                         <td>@if($item->is_deposit == 2) <p>Kosong</p> @elseif($item->is_deposit == 1) <b>Deposit + Rp. 15.000 @if($item->is_addon == 1) + {{ $item->note }} @endif</b> @elseif($item->is_deposit == 3) <b>Deposit</b>@elseif($item->is_deposit == 4) <b>Normal +Rapel dengan bulan Mei</b> @else Normal @endif</td>
                         
